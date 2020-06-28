269be02f0972a42ac6da1d98d12ecb2c4000a662
kakusuke0605 <563dai4044@gmail.com>
2020-06-28 20:57:38 +0900

    commit

--- Register/config.py
+++ Register/config.py
@@ -8,10 +8,10 @@ class DevelopmentConfig:
 
     #MySQL
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8mb4'.format(**{
-        'user': os.getenv('DB_USER', 'b7e2f907807773'),
-        'password': os.getenv('DB_PASSWORD', 'fd52072d'),
+        'user': os.getenv('DB_USER', 'b5ce340141e1f5'),
+        'password': os.getenv('DB_PASSWORD', '969a6a56'),
         'host': os.getenv('DB_HOST', 'us-cdbr-east-02.cleardb.com'),
-        'database': os.getenv('DB_DATABASE', 'heroku_f05ad4fafd515a1')
+        'database': os.getenv('DB_DATABASE', 'heroku_9de8a2b86d37394')
     })
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
